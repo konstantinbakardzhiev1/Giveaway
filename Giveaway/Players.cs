c771dab1f3dcf32ffdaa0ed81b5a4852dc2e79e6
konstantinbakardzhiev1 <konstantinbakardzhiev1@gmail.com>
2024-02-09 11:57:15 +0200

    Add project files.

---
+++ Giveaway/Players.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giveaway
+{
+    public class Players
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Number { get; set; }
+        public bool IsFollower { get; set; }
+    }
+}
---
+++ Giveaway/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Giveaway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            bool isFollower;
+
+            Random random = new Random();
+
+            List<Players> playersList = new List<Players>();
+
+            while (input != "end")
+            {
+                string status = Console.ReadLine();
+                string[] inputParts = input.Split(" ");
+                string firstName = inputParts[0];
+                string lastName = inputParts[1];
+                int number = int.Parse(inputParts[2]);
+
+
+                Players players = new Players()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Number = number
+                };
+
+                if (status == "yes")
+                {
+                    isFollower = true;
+                    playersList.Add(players);
+                }
+                else
+                {
+                    isFollower = false;
+
+                }
+
+                input = Console.ReadLine();
+            }
+            Players randomPlayer = GetRandomPlayer(playersList, random);
+
+
+            Console.WriteLine($"Winner: Number {randomPlayer.Number} - {randomPlayer.FirstName} {randomPlayer.LastName}");
+        }
+
+        static Players GetRandomPlayer(List<Players> playersList, Random random)
+        {
+            int randomIndex = random.Next(0, playersList.Count);
+            return playersList[randomIndex];
+        }
+    }
+    
+}
